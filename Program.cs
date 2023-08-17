2c3328566ade296fa722b31994c54544baeafae5
austinbhan <austinbhan@outlook.com>
2023-08-17 16:17:29 -0700

    Soup practice in progress

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Runtime.ConstrainedExecution;
+
+Food currentFood = Food.Soup;
+Taste currentTaste = Taste.Hot;
+Ingredient currentIngredient = Ingredient.Mushrooms;
+
+var finalResult = (currentFood, currentTaste, currentIngredient);
+Console.WriteLine(finalResult);
+
+enum Food {
+    Soup,
+    Stew,
+    Gumbo
+}
+
+enum Taste {
+    Hot, 
+    Salty, 
+    Sweet
+}
+
+enum Ingredient {
+    Mushrooms,
+    Chicken, 
+    Carrots,
+    Potatoes
+}
