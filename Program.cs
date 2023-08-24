e869c663fff0bca3c8c4692b0d6c00152246a178
austinbhan <austinbhan@outlook.com>
2023-08-23 18:22:51 -0700

    Remove default values

--- Program.cs
+++ Program.cs
@@ -2,9 +2,7 @@
 
 using System.Runtime.ConstrainedExecution;
 
-Food currentFood = Food.Soup;
-Taste currentTaste = Taste.Hot;
-Ingredient currentIngredient = Ingredient.Mushrooms;
+
 
 var finalResult = (currentFood, currentTaste, currentIngredient);
 Console.WriteLine(finalResult);
