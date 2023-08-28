8b09ec3ebe9dda2ccfbb14d35950b69cf588f9be
austinbhan <austinbhan@outlook.com>
2023-08-28 15:40:36 -0700

    More classes

--- Program.cs
+++ Program.cs
@@ -1,19 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Reflection.Emit;
-using System.Runtime.ConstrainedExecution;
-
-Score score = new Score("R2D2", 12420, 15);
-class Score {
-    public string name;
-    public int points;
-    public int level;
-
-    public Score(string n, int p, int l) 
-    {
-        name = n;
-        points = p;
-        level = l;
-    }
-    public bool EarnedStar() => (points/level) > 1000;
-}
