9a4af1582c36fb2075e3e8328566d0ac6dfe7bb4
austinbhan <austinbhan@outlook.com>
2023-08-29 14:31:27 -0700

    Remove class notes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// Define arrow class
+// with arrowhead, fletching, length (use enumerations)
+
+// arrow = 10, wood = 3, obsidian = 5
+// fletching: plastic= 10, turkey feathers = 5, goose = 3;
+// shaft: 0.05g per centimeter
+
+// allow user to pick arrowhead, fletching, length and create new arrow instances
+
+// add getCost method, returns cost as float based on numbers
+
+// -User experience-
+
+// Input arrowhead
+// Input fletching
+// Input length
+// Return entire type with price: 
+
+// -I need-
+// User creates a class instance
+// Creator creates class parameters 
