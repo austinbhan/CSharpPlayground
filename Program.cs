c447ce50c060b50ddaabab34cd34181b2916503d
austinbhan <austinbhan@outlook.com>
2023-08-31 20:16:03 -0700

    Class with basic constructor

--- Program.cs
+++ Program.cs
@@ -1,49 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// Define arrow class
-// with arrowhead, fletching, length (use enumerations)
+Score a = new Score("Bob", 12420, 15);
 
-// arrow = 10, wood = 3, obsidian = 5
-// fletching: plastic= 10, turkey feathers = 5, goose = 3;
-// shaft: 0.05g per centimeter
+Console.WriteLine(a);
 
-// allow user to pick arrowhead, fletching, length and create new arrow instances
+class Score
+{
+    public string _name;
+    public int _points;
+    public int _level;
 
-// add getCost method, returns cost as float based on numbers
-
-// -User experience-
-
-// Input arrowhead
-// Input fletching
-// Input length
-// Return entire type with price: 
-
-// -I need-
-// User creates a class instance
-// Creator creates class parameters 
-
-using System.Runtime.InteropServices;
-
-Arrow arrow = new Arrow(); // Create a new instance whose type is arrow, and to store it 
-class Arrow {
-    public ArrowHeadType _arrowheadType; // Create values into existence
-    public FletchingType _fletchingType;
-    public float _length;
-
-    public Arrow(ArrowHeadType arrowHeadType, FletchingType fletchingType, float length)  // Assign values
+    public Score(string name, int points, int level) 
     {
-        _arrowheadType = arrowHeadType;
-        _fletchingType = fletchingType;
-        _length = length;
+        _name = name;
+        _points = points;
+        _level = level;
     }
 
-    public Arrow() // Define default values
-    {
-        _arrowheadType = ArrowHeadType.Steel;
-        _fletchingType = FletchingType.GooseFeather;
-        _length = 75;
-    }
+    public bool EarnedStar() => (_points / _level) > 1000;
 }
-
-enum ArrowHeadType { Steel, Wood, Obsidian }
-enum FletchingType { Plastic, TurkeyFeather, GooseFeather }
