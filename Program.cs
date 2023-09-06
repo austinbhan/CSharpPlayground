939b5e1ad4a59c53ba08fd2f003426345d79942a
austinbhan <austinbhan@outlook.com>
2023-09-06 16:10:34 -0700

    Soup enum exercise

--- Program.cs
+++ Program.cs
@@ -1,21 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("What is your input?");
-string input = Console.ReadLine();
+Soup current = Soup.Potato;
 
-Score a = new Score(input, 12420, 15);
-Console.WriteLine(a._name);
 
-class Score
+while(true)
 {
-    public string _name;
-    public int _points;
-    public int _level;
+string userInput = Console.ReadLine();
 
-    public Score(string name, int points, int level) 
-    {
-        _name = name;
-        _points = points;
-        _level = level;
-    }
+
+if (userInput == "Chicken")
+{
+    current = Soup.Chicken;
+    Console.WriteLine(current);
+
 }
+    if (userInput == "Potato")
+    {
+
+    current = Soup.Potato;
+    Console.WriteLine(current);
+    }
+    if (userInput == "Banana")
+    current = Soup.Banana;
+    Console.WriteLine(current);
+
+}
+
+
+enum Soup { Potato, Chicken, Banana } 
+
